01ca3048b5d558dc1021c46cb85015c37f4081d9
Chander Ramesh <chander@wonderly.com>
2026-06-02 16:07:11 -0700

    internal-tools 0.1.15

--- Formula/internal-tools.rb
+++ Formula/internal-tools.rb
@@ -1,9 +1,9 @@
 class InternalTools < Formula
   desc "Shared local auth helper for Wonderly internal IAP apps"
   homepage "https://github.com/wonderlydotcom/internal-tools-cli"
-  url "https://github.com/wonderlydotcom/homebrew-tap/releases/download/internal-tools-0.1.14/internal-tools-0.1.14.tar.gz"
-  sha256 "2b4f2fabb870ddf72e644399fcdc9c7c970ea82fa0339e25995eda56d902e5f8"
-  version "0.1.14"
+  url "https://github.com/wonderlydotcom/homebrew-tap/releases/download/internal-tools-0.1.15/internal-tools-0.1.15.tar.gz"
+  sha256 "4708e939d6b6eb943ea16eee0fce5c20aa192b75fff0781a6a7b752d30484a69"
+  version "0.1.15"
 
   depends_on "python@3.12"
 
